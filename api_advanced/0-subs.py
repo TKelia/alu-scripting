72814fc844fccd6ad8e4edc7ea80ec2a99e3d6b6
TKelia <k.tesi@alustudent.com>
2024-07-05 08:34:42 -0700

    how many subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,7 +5,6 @@ Fetches the number of subscribers for a given Reddit subreddit.
 
 import requests
 
-
 def fetch_subscriber_count(subreddit):
     """Fetches subscriber count from Reddit API"""
     user_agent = 'Mozilla/5.0'
@@ -16,12 +15,13 @@ def fetch_subscriber_count(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
+        print("OK")
         return 0
 
-    try:
-        data = response.json()
-        if 'data' not in data or 'subscribers' not in data['data']:
-            return 0
-        return data['data']['subscribers']
-    except ValueError:
+    data = response.json()
+    if 'data' not in data or 'subscribers' not in data['data']:
+        print("OK")
         return 0
+    
+    print("OK")
+    return data['data']['subscribers'
